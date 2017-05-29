1eda4ffccbac3fe4da348c140d1910a5186968a9
MarioGryth <mgryth@gmail.com>
2017-05-29 17:52:37 +0900

    My first commit

--- FlashCard2/ViewController.swift
+++ FlashCard2/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonBool = false
+    var nrTimesPressed = 0
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textLabel.text = String(nrTimesPressed)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+    
+        if !buttonBool {
+            nrTimesPressed += 1
+            textLabel.text = String(nrTimesPressed)
+            
+            
+            if nrTimesPressed > 10 {
+                textLabel.text = "BUTTON MANIAC"
+                buttonBool = true
+                
+            }
+        }
+        
+        
+    }
 
 }
 
